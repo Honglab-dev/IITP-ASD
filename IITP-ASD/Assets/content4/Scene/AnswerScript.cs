a8aa1939a84383649e091386e9afa0b180dc3595
kangyurim <108712616+kangyurim@users.noreply.github.com>
2023-01-30 16:22:42 +0900

    combine all file
    combine all file

---
+++ IITP-ASD/Assets/content4/Scene/AnswerScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnswerScript : MonoBehaviour
+{
+    public bool isCorrect = false;
+    public QuizManager quizManager;
+
+    //public Color startColor;
+
+    public void Answer()
+    {
+        if(isCorrect)
+        {
+            //GetComponent<Iage>().color = color.green;
+            Debug.Log("맞았습니다.");
+            quizManager.correct();
+        }
+        else
+        {
+            //GetComponent<Image>().color = color.red;
+            Debug.Log("틀렸습니다.");
+            quizManager.wrong();
+        }
+    }
+}
---
+++ IITP-ASD/Assets/content4/Scene/QuizManager.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class QuizManager : MonoBehaviour
+{
+    public List<QuestionAndAnswers> QnA;
+    public GameObject[] options;
+    public int currentQuestion;
+
+    public Text QuestionText;
+    public Text ScoreText;
+
+    public GameObject QuizPanel;
+    public GameObject GoPanel;
+
+    int totalQuestion = 0;
+    public int score;
+
+    // 시작하면 makeQuestion() 함수 호출
+    private void Start()
+    {
+        totalQuestion = QnA.Count;
+        GoPanel.SetActive(false);
+        makeQuestion();
+    }
+
+    // 문제 갯수만큼 랜덤 생성
+    void makeQuestion()
+    {
+        if (QnA.Count > 0)
+        {
+            currentQuestion = Random.Range(0, QnA.Count);
+            QuestionText.text = QnA[currentQuestion].Question;
+            SetAnswers();
+            //QnA.RemoveAt(currentQuestion);
+        }
+        else{
+            Debug.Log("문제를 다 풀었습니다.");
+            GameOver();
+        }
+    }
+
+    void SetAnswers()
+    {
+        for (int i=0;i < options.Length; i++)
+        {
+            //options[i].GetComponent<Image>().color = options[i].GetComponent<AnswerScript>().startColor;
+            options[i].GetComponent<AnswerScript>().isCorrect = false;
+            options[i].transform.GetChild(0).GetComponent<Image>().sprite = QnA[currentQuestion].Answers[i];
+
+            if (QnA[currentQuestion].CorrectAnswer == i+1)
+            {
+                options[i].GetComponent<AnswerScript>().isCorrect = true;
+            }
+        }
+    }
+
+    public void correct()
+    {
+        score += 1;
+        QnA.RemoveAt(currentQuestion);
+        makeQuestion();
+        //StartCoroutine(waitForNext());
+    }
+
+    public void wrong()
+    {
+
+        //QnA.RemoveAt(currentQuestion);
+        //makeQuestion();
+        //StartCoroutine(waitForNext());
+    }
+
+    void GameOver()
+    {
+        QuizPanel.SetActive(false);
+        GoPanel.SetActive(true); 
+        ScoreText.text = score + "개 맞았습니다.";
+    }
+
+    public void retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    
+    //IEnuerator waitForNext()
+    //{
+    //    yield return new waitForNext(1);
+    //    makeQuestion();
+    //}
+
+}
