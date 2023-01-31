266c03b40999f08af6b61753758472ca3a851f35
HYUN <8135zxcvbn@gmail.com>
2023-01-31 10:29:19 +0900

    hyun activity2

---
+++ IITP-ASD/Assets/content4/Script/AnimationEvent.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+//Handler
+public class AnimationEvent : MonoBehaviour
+{ 
+ //#region~#endregion : CodeBlock
+    #region Fields
+    #endregion Fields
+
+    #region Members
+    private Animator m_Animator;
+    public GameObject Fruit1;
+    public GameObject Fruit2;
+    public GameObject Fruit3;
+    public GameObject Fruit4;
+
+    #endregion Members
+
+    #region Methods
+
+    //Awake()->Start()
+    void Awake()
+    {
+        m_Animator = GetComponent<Animator>();
+        Fruit1.gameObject.SetActive(false);
+        Fruit2.gameObject.SetActive(false);
+        Fruit3.gameObject.SetActive(false);
+        Fruit4.gameObject.SetActive(false);
+    }
+
+    public void EnterNextScene()
+    {
+        //playanimation
+        m_Animator.Play("GuestCharcterMove");
+    }
+
+    public void OnEnterNextScene()
+    {
+        //After animation ends
+        Debug.Log("Active");
+        Fruit1.gameObject.SetActive(true);
+        Fruit2.gameObject.SetActive(true);
+        Fruit3.gameObject.SetActive(true);
+        Fruit4.gameObject.SetActive(true);
+    }
+
+    #endregion Methods
+}
