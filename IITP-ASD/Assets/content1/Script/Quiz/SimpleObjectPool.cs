5722d00c0a668975f6bef2c28058c954189b1d94
HYUN <8135zxcvbn@gmail.com>
2023-03-06 18:56:46 +0900

    edit script

--- IITP-ASD/Assets/content1/Script/Quiz/SimpleObjectPool.cs
+++ IITP-ASD/Assets/content1/Script/Quiz/SimpleObjectPool.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//object pooling class
 public class SimpleObjcetPool : MonoBehaviour
 {
-   
-    void Start()
-    {
+    // the prefab that this object pool returns instances of //instanceof -> check type?
+    public GameObject prefab;
 
-        
+    //collection of currently inactive instances of the prefab
+    private Stack<GameObject> inactiveInstances = new Stack<GameObject> (); 
+
+    public GameObject GetObject()
+    {
+        GameObject spawnedGameObject;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
